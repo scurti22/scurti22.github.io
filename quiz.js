de29927cf50c192a6ebb41a8443e20c00aa944e7
scurti22 <scurti22@students.kennesaw.edu>
2025-04-30 22:04:14 -0400

    updates moving files

--- quiz.js
+++ quiz.js
@@ -1,7 +1,8 @@
+//defines funtion and starts score at 0 defines result as empty string
 function gradequiz() {
     let score = 0;
     let result= "";
-
+// defines the answers fro each question
     const answers = {
         q1: "c",
         q2: "a",
@@ -10,7 +11,7 @@ function gradequiz() {
         q5: ["a", "c", "d"]
 
     };
-    
+    // checks if user input is correct and adds to score if true
     for (let i = 1; i <= 3; i++) {
         const input = document.querySelector(`input[name="q${i}"]:checked`);
         if (input && input.value === answers[`q${i}`]) {
@@ -20,7 +21,6 @@ function gradequiz() {
             result += `Q${i} Wrong\n`;
         }
     }
-
     const q4 = document.querySelector('input[name="q4"]').value.trim().toLowerCase();
     if (q4 === answers.q4.toLowerCase()) {
         score++;
@@ -37,7 +37,10 @@ function gradequiz() {
     } else {
         result += "Q5 Wrong\n";
     }
-
+// displays the score and if user passed or failed quiz
     result += `Score: ${score}/5 — ${score >= 3 ? "PASS" : "FAIL"}`;
-    console.log(result);
+    document.getElementById("result").textContent = result;
 }
+
+
+
